7e5275ecca6a69966fb236deefbc024f46d72eff
Rifatul Islam <rifatulcse@gmail.com>
2024-11-13 14:48:53 +0600

    updated

--- src/components/News/RecentShip.jsx
+++ src/components/News/RecentShip.jsx
@@ -14,12 +14,12 @@ const cardData = [
 
 const RecentShip = () => {
   return (
-    <div className='mx-4 lg:mx-40 lg:mb-40 mb-20 mt-10'>
+    <div className='mx-4 lg:mx-40 lg:mb-40 mb-20 mt-10 z-10'>
       <div className='grid gap-10 lg:gap-6 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
         {cardData.map((card) => (
           <div
             key={card.id}
-            className='relative overflow-hidden rounded-lg w-4/5 lg:h-[160%] mx-auto hover:brightness-150 hover:scale-105 duration-1000'
+            className='relative overflow-hidden rounded-lg w-4/5 lg:h-[160%] mx-auto hover:brightness-150 hover:scale-105 duration-1000 z-50'
           >
             {/* Use Link to wrap the image and text for navigation */}
             <Link to={`/news-details/${card.id}`} className='block'>
@@ -27,14 +27,14 @@ const RecentShip = () => {
               <img
                 src={card.image}
                 alt={card.title}
-                className='w-full h-full object-cover '
+                className='w-full h-full object-cover'
               />
 
               {/* Dark Overlay for Text Visibility */}
               <div className='absolute top-0 left-0 w-full h-full bg-black bg-opacity-40 flex justify-start items-end p-5'>
                 <div className='text-white text-start'>
-                  <h3 className='m-0 text-xl sm:text-2xl md:text-2xl lg:text-xl'>{card.title}</h3>
-                  <p className='ml-2 m-0 text-sm sm:text-base md:text-lg lg:text-sm'>{card.date}</p>
+                  <h3 className='m-0 text-xs sm:text-2xl md:text-2xl lg:text-xl'>{card.title}</h3>
+                  <p className='ml-2 m-0 text-sm  md:text-lg lg:text-sm'>{card.date}</p>
 
                 </div>
               </div>
